beea9321c00e93ffa9c4c768fddb0cb8a2d034d6
Burakcan Sezgin <burakcan.sezgin@gmail.com>
2020-02-22 19:26:29 +0300

    firstCommit

---
+++ app/src/main/java/com/example/recyclerviews/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.recyclerviews
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /**
+         * Adapter Nedir ?
+         *
+         * */
+    }
+}
